6b68fc36c4822d3487ab446184d6c1e063dd845c
DongYeop <s01054830455@gmail.com>
2024-12-09 01:02:42 +0900

    코드 다듬기[6-5] 기능 유지보수하기(2) - 알고리즘 교체하기
    - 재귀를 사용한 DFS도 결국 Stack 이다.
        - 스레드마다 생기는 스택 영역에는,
        함수를 호출할 때마다 frame이 쌓인다.
        - frame은 지역 변수, 연산을 위한 정보 등을 담고 있다.
        - 스택 영역은 결국 크기가 제한되어 있다.

--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/GameBoard.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/GameBoard.java
@@ -6,11 +6,12 @@ import cleancode.minesweeper.tobe.minesweeper.board.cell.Cells;
 import cleancode.minesweeper.tobe.minesweeper.board.cell.EmptyCell;
 import cleancode.minesweeper.tobe.minesweeper.board.cell.LandMineCell;
 import cleancode.minesweeper.tobe.minesweeper.board.cell.NumberCell;
-import cleancode.minesweeper.tobe.minesweeper.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.minesweeper.board.position.CellPosition;
 import cleancode.minesweeper.tobe.minesweeper.board.position.CellPositions;
 import cleancode.minesweeper.tobe.minesweeper.board.position.RelativePosition;
+import cleancode.minesweeper.tobe.minesweeper.gamelevel.GameLevel;
 import java.util.List;
+import java.util.Stack;
 
 public class GameBoard {
 
@@ -46,7 +47,7 @@ public class GameBoard {
             changeGameStatusToLose();
             return;
         }
-        openSurroundedCells(cellPosition);
+        openSurroundedCells2(cellPosition);
         checkIfGameIsOver();
     }
 
@@ -154,6 +155,37 @@ public class GameBoard {
 
         List<CellPosition> surroundedPositions = calcaulteSurroundedPositions(cellPosition, getRowSize(), getColSize());
         surroundedPositions.forEach(this::openSurroundedCells);
+    }
+
+    private void openSurroundedCells2(CellPosition cellPosition) {
+        Stack<CellPosition> stack = new Stack<>();
+        stack.push(cellPosition);
+
+        while (!stack.isEmpty()) {
+            openAndPushCellAt(stack);
+        }
+    }
+
+    private void openAndPushCellAt(Stack<CellPosition> stack) {
+        CellPosition currentCellPosition = stack.pop();
+        if (isOpendCell(currentCellPosition)) {
+            return;
+        }
+        if (isLandMineCellAt(currentCellPosition)) {
+            return;
+        }
+
+        openOneCellAt(currentCellPosition);
+
+        if (doesCellHaveLandMineCount(currentCellPosition)) {
+            return;
+        }
+
+        List<CellPosition> surroundedPositions = calcaulteSurroundedPositions(currentCellPosition, getRowSize(),
+                getColSize());
+        for (CellPosition surroundedPosition : surroundedPositions) {
+            stack.push(surroundedPosition);
+        }
     }
 
     private void openOneCellAt(CellPosition cellPosition) {
